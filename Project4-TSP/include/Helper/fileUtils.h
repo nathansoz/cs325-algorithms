6fef02e0e30e429754267acc52a272344b5b4819
Nathan Sosnovske <nathan@sosnovske.com>
2015-08-09 14:50:12 -0700

    Add file processing helpers.

--- Project4-TSP/include/Helper/fileUtils.h
+++ Project4-TSP/include/Helper/fileUtils.h
@@ -6,5 +6,7 @@
 namespace Helper
 {
     std::vector<std::string>* readLinesFromFile(std::ifstream&);
-    std::vector<int> csvToInt(std::string&);
+    std::vector<int> spaceToInt(std::string&);
+    std::vector<std::vector<int>> vectorizeLines(std::vector<std::string>*);
+    boost::adjacency_list* createGraph(std::vector<std::string>*);
 }
--- Project4-TSP/src/Helper/fileUtils.cpp
+++ Project4-TSP/src/Helper/fileUtils.cpp
@@ -4,6 +4,8 @@
 #include <vector>
 #include <iostream>
 
+#include <boost/graph/adjacency_list.hpp>
+
 #include "../../include/Helper/fileUtils.h"
 
 std::vector<std::string>* Helper::readLinesFromFile(std::ifstream& inFromFile)
@@ -20,14 +22,21 @@ std::vector<std::string>* Helper::readLinesFromFile(std::ifstream& inFromFile)
 
 }
 
+/*
+std::vector<std::vector<int>> Helper::vectorizeLines(std::vector<std::string>*)
+{
+
+}
+
+ */
 
 //This function will be useful, it just needs to be converted to space separated values
-std::vector<int> Helper::csvToInt(std::string &convertString)
+std::vector<int> Helper::spaceToInt(std::string &convertString)
 {
     std::vector<int> returnVector;
     const char *cstrConvertString = convertString.c_str();
 
-    char* tmpString = strtok((char*)cstrConvertString, " ,");
+    char* tmpString = strtok((char*)cstrConvertString, " ");
     while(tmpString != NULL)
     {
         int tmp = atoi(tmpString);
@@ -37,5 +46,14 @@ std::vector<int> Helper::csvToInt(std::string &convertString)
 
     return returnVector;
 }
+
+/*
+
+boost::adjacency_list* Helper::createGraph(std::vector<std::string>*)
+{
+
+}
+
+ */
 
 
